c9d5ffe590083b2b1fd074647c6d86b3188ca559
chinguyen21 <nguyen.thimai@northeastern.edu>
2020-11-24 19:17:22 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
+
 class Triangle
   # write code here
+  attr_reader :side_1, :side_2, :side_3
+
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+  
+  def kind
+
+    array = [@side_1, @side_2, @side_3]
+    max = array.max
+    if array.any? {|side| side <= 0}
+        raise TriangleError
+    elsif array.sum - max <= max
+        raise TriangleError
+    else
+      if @side_1 == @side_2 && @side_2 == @side_3
+        :equilateral
+      elsif (@side_1 == @side_2 && @side_1 != @side_3) ||
+        (@side_1 == @side_3 && @side_1 != @side_2) ||
+        (@side_2 == @side_3 && @side_2 != @side_1)
+        :isosceles
+      else
+        :scalene
+      end
+    end
+    
+  end
+  
+
+  class TriangleError < StandardError
+  end
+
 end
+
+
